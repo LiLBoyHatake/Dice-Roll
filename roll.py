2c94dfe0c199c6849110a7cf43a29520722a94c9
Hatake <75223711+LiLBoyHatake@users.noreply.github.com>
2020-12-01 18:09:11 -0500

    Process of moving to tkinter

--- roll.py
+++ roll.py
@@ -1,15 +1,36 @@
 import random
 import time
+import tkinter as tk
 min = 1
 max = 6
 
-roll_again = "yes"
 
-while roll_again == "yes" or roll_again == "y":
-    print ("You see me rollin")
-    time.sleep(3)
-    print ("Your results are - ")
-    print (random.randint(min, max))
-    print (random.randint(min, max))
-    time.sleep(1)
-    roll_again = input('Roll again? - ')
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+    
+    def create_widgets(self):
+        self.die_roll = tk.Button(self)
+        self.die_roll['text'] = 'Roll the Dice'
+        self.die_roll['command'] = self.die_roll
+        self.die_roll.pack(side='top')
+    
+
+
+    def die_roll(self):
+        print('Rolling')
+        time.sleep(2)
+        print('You got - ')
+        print(ranom.randit(min, max))
+        time.sleep(1.5)
+        print('If you want to play agian press the button.')
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
+
