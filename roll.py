dcfe7fdc82e3c2c584369ecd8a23f9269f8d8f11
Hatake <75223711+LiLBoyHatake@users.noreply.github.com>
2020-12-01 17:54:18 -0500

    Added time.sleep, and changed print

--- roll.py
+++ roll.py
@@ -1,13 +1,15 @@
 import random
+import time
 min = 1
 max = 6
 
 roll_again = "yes"
 
 while roll_again == "yes" or roll_again == "y":
-    print ("Rolling the dices...")
-    print ("The values are....")
+    print ("You see me rollin")
+    time.sleep(3)
+    print ("Your results are - ")
     print (random.randint(min, max))
     print (random.randint(min, max))
-
+    time.sleep(1)
     roll_again = input('Roll again? - ')
